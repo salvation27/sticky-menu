a9fc81541a29abadc99bf06b041d5c5458ff39f1
Salvation27 <nikolayzalipykin@gmail.com>
2022-06-18 12:18:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTransform, motion, useViewportScroll } from 'framer-motion';
+import './style.css';
 
-function App() {
+import { TheHeader } from './TheHeader';
+
+export default function App() {
+  const { scrollY } = useViewportScroll();
+
+  const offsetY = [0, 300];
+  const marginTop = useTransform(scrollY, offsetY, offsetY);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: 'relative' }}>
+      <TheHeader offsetY={offsetY} scrollY={scrollY} />
+      <motion.div
+        className="section"
+        style={{ backgroundColor: 'maroon', marginTop }}
+      >
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </motion.div>
+      <div className="section" style={{ backgroundColor: 'peru' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ea,
+        libero aliquam explicabo aliquid modi sint quae obcaecati voluptatibus
+        voluptatem.
+      </div>
     </div>
   );
 }
-
-export default App;
